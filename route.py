5a625d5f8bf01951352190e0fe377bacf20720ff
Aleksander Kurczyk <akurczyk000@gmail.com>
2019-04-01 17:17:34 +0200

    Changing route values to continuous

--- route.py
+++ route.py
@@ -17,12 +17,12 @@ class Route:
         return ', '.join([state.capital for state in self.states])
 
     def print(self):
-        table = PrettyTable(['#', 'Name', 'Capital'], header_style='upper')
+        table = PrettyTable(['#', 'Name', 'Capital', 'All electoral votes'], header_style='upper')
         table.align = 'l'
 
         i = 1
         for state in self.states:
-            table.add_row([i, state.name, state.capital])
+            table.add_row([i, state.name, state.capital, state.electoral_votes])
             i += 1
 
         print(table)
@@ -37,6 +37,33 @@ class Route:
             tmp_states.remove(current_state)
 
     def calculate_value(self):
+        def dec_support(states_supports, time, excluded_state=None):
+            for state in states_supports:
+                if state != excluded_state:
+                    states_supports[state] = self.dec_support(states_supports[state], time)
+
+        states_supports = {}
+        for state in self.states:
+            states_supports[state] = self.INITIAL_SUPPORT
+
+        for i in range(len(self.states)):
+            state = self.states[i]
+
+            dec_support(states_supports, self.VISIT_TIME, state)
+            states_supports[state] = self.inc_support(states_supports[state], self.VISIT_TIME)
+
+            if i != len(self.states) - 1:
+                next_state = self.states[i + 1]
+                travel_time = state.get_travel_time_to(next_state)
+                dec_support(states_supports, travel_time)
+
+        value = 0
+        for state in states_supports:
+            value += states_supports[state] * state.electoral_votes
+
+        self.value = value
+
+    def calculate_real_value(self):
         def dec_support(states_supports, time, excluded_state=None):
             for state in states_supports:
                 if state != excluded_state:
